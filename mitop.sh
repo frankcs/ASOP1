0e778f373658739b73e9607195a3b19e9d951263
Frank <frank90cs@gmail.com>
2016-11-22 20:08:46 +0100

    Improving read from stat and creating data array

--- mitop.sh
+++ mitop.sh
@@ -19,34 +19,26 @@ echo Total Memory: $MEMTOTAL
 echo Used Memory: $(($MEMTOTALNUM - $MEMFREENUM)) kB
 echo Free Memory: $MEMFREE
 
-OUTPUT="PID USER PR VIRT S %CPU %MEM\n\n"
+#OUTPUT="PID USER PR VIRT S CPUTIME %MEM\n"
+
+declare -a pdata
 
 for pid in "${pids[@]}"
 do
-    USERID=`get_prop_from_file /proc/$pid/status Uid|awk '{print $1}'`
-    PRIORITY=`awk '{print $18}' /proc/$pid/stat`
-    VIRTUALMEM=`awk '{print $23}' /proc/$pid/stat`
-    STATE=`awk '{print $3}' /proc/$pid/stat`
+	if [ -d "/proc/$pid/" ];then
+    USERID=`get_prop_from_file /proc/$pid/status Uid|awk '{print $1}'`   
+    read PRIORITY VIRTUALMEM STATE UTIME STIME<<< $(awk '{print $18" "$23" "$3" "$14" "$15}' /proc/$pid/stat)
+    TOTALTIME=$(($UTIME + $STIME))    
 
-    UPTIME=`awk '{print $1}' /proc/uptime`
-    UPTIME=`extract_number $UPTIME`
+    MEMPERCENT=`bc <<< "scale=2; $VIRTUALMEM / 1024*100 / $MEMTOTALNUM"`
+    pdata[$pid]="$pid $USERID $PRIORITY $VIRTUALMEM $STATE $TOTALTIME $MEMPERCENT"
+    echo ${pdata[$pid]}
 
-    STARTTIME=`awk '{print $22}' /proc/$pid/stat`
-    UTIME=`awk '{print $14}' /proc/$pid/stat`
-    STIME=`awk '{print $15}' /proc/$pid/stat`
-    HERTZ=`getconf CLK_TCK`
-    TOTALTIME=$(($UTIME + $STIME))
-    SECONDS=$(($UPTIME - ($STARTTIME / $HERTZ)))
-#this is crap
-    CPUPERCENT=$((100 * $TOTALTIME / $HERTZ / $SECONDS))
-
-    MEMPERCENT=$(($VIRTUALMEM / 1024*100 / $MEMTOTALNUM))
-
-
-    OUTPUT="$OUTPUT\n$pid $USERID $PRIORITY $VIRTUALMEM $STATE $CPUPERCENT $MEMPERCENT"
+    #OUTPUT="$OUTPUT\n$pid $USERID $PRIORITY $VIRTUALMEM $STATE $TOTALTIME $MEMPERCENT\n"
+	fi
 done
 
-echo -ne $OUTPUT | column -t
+#echo -ne $OUTPUT | column -t
 
 
 
